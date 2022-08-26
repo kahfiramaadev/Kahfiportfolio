3f3024de8eef137e0a0dd4c6316a195876c501db
kahfiramaadev <kahfirmdn84@gmail.com>
2022-08-27 02:05:56 +0700

    First commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi,i'm Muhammad kahfi ramadhan</span> i am 19 years old and my formal education is at SMK Negri 3 Depok and majoring in Computer Network Engineering. I work with a good and structural planning with my team. There's no matter if i join with an environment which has many differnt characters witin. And i'm very intersted to study and explore sometihng new especially have some correlation with my job. I can work indepently and in a team. Responsible, self, confidence,friendly, humble, work hard, high commitment, positive and paroactive mental attitude aresome of my character.Please give me an opportunity to meet with you to disscus my qualification and the role imight play in your company. You can reach me at the above email address or at my mobile phone at any time. Thank you for your consideration."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
